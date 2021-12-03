15ef6e9b60c9cc4d453cc6b7e921705454e202e8
Arthur Matthys <arthur@matthys.fr>
2021-12-03 21:57:22 +0100

    Architecture created

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+#![feature(array_zip)]
 mod lib;
-use lib::matrix::Matrix;
-use lib::vector::Vector;
 
 mod ex00;
+mod ex01;
+mod ex02;
+mod ex03;
+mod ex04;
+mod ex05;
+mod ex06;
+mod ex07;
+mod ex08;
+mod ex09;
+mod ex10;
+mod ex11;
+mod ex12;
+mod ex13;
+mod ex14;
+mod ex15;
 
 pub fn main() {
-    let a = Matrix::from([[1., 2.], [3., 4.]]);
     return;
 }
