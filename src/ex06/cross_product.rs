4c53c483db768649a3544e8417c76d39c724c7d3
Arthur Matthys <arthur@matthys.fr>
2021-12-04 23:02:57 +0100

    Cross product of two 3D vectors

---
+++ src/ex06/cross_product.rs
@@ -0,0 +1,83 @@
+use std::ops::{Add, Mul, Sub};
+
+use crate::lib::{scalar::Scalar, vector::Vector};
+
+pub fn cross_product<S>(u: Vector<S, 3>, v: Vector<S, 3>) -> Vector<S, 3>
+where
+    S: Scalar + Mul<Output = S> + Sub<Output = S> + Add<Output = S>,
+{
+    Vector([
+        u.0[1].clone() * v.0[2].clone() - u.0[2].clone() * v.0[1].clone(),
+        u.0[2].clone() * v.0[0].clone() - u.0[0].clone() * v.0[2].clone(),
+        u.0[0].clone() * v.0[1].clone() - u.0[1].clone() * v.0[0].clone(),
+    ])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cross_product_00() {
+        let u = Vector([0., 0., 1.]);
+        let v = Vector([1., 0., 0.]);
+        let res = Vector([0., 1., 0.]);
+        assert!(cross_product(u, v) == res);
+    }
+    #[test]
+    fn test_cross_product_01() {
+        let u = Vector([1., 2., 3.]);
+        let v = Vector([4., 5., 6.]);
+        let res = Vector([-3., 6., -3.]);
+        assert!(cross_product(u, v) == res);
+    }
+    #[test]
+    fn test_cross_product_02() {
+        let u = Vector([4., 2., -3.]);
+        let v = Vector([-2., -5., 16.]);
+        let res = Vector([17., -58., -16.]);
+        assert!(cross_product(u, v) == res);
+    }
+    #[test]
+    fn test_cross_product_03() {
+        let u = Vector([0., 0., 0.]);
+        let v = Vector([0., 0., 0.]);
+        let res = Vector([0., 0., 0.]);
+        assert!(cross_product(u, v) == res);
+    }
+    #[test]
+    fn test_cross_product_04() {
+        let u = Vector([1., 0., 0.]);
+        let v = Vector([0., 0., 0.]);
+        let res = Vector([0., 0., 0.]);
+        assert!(cross_product(u, v) == res);
+    }
+    #[test]
+    fn test_cross_product_05() {
+        let u = Vector([1., 0., 0.]);
+        let v = Vector([0., 1., 0.]);
+        let res = Vector([0., 0., 1.]);
+        assert!(cross_product(u, v) == res);
+    }
+    #[test]
+    fn test_cross_product_06() {
+        let u = Vector([8., 7., -4.]);
+        let v = Vector([3., 2., 1.]);
+        let res = Vector([15., -20., -5.]);
+        assert!(cross_product(u, v) == res);
+    }
+    #[test]
+    fn test_cross_product_07() {
+        let u = Vector([1., 1., 1.]);
+        let v = Vector([0., 0., 0.]);
+        let res = Vector([0., 0., 0.]);
+        assert!(cross_product(u, v) == res);
+    }
+    #[test]
+    fn test_cross_product_08() {
+        let u = Vector([1., 1., 1.]);
+        let v = Vector([1., 1., 1.]);
+        let res = Vector([0., 0., 0.]);
+        assert!(cross_product(u, v) == res);
+    }
+}
